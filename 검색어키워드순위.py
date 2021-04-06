d23e2e72cf15506fdad67f629a9e936cd2b9d02c
하승우 <gkgktmd@naver.com>
2021-04-06 12:12:47 +0900

    등->순위 네이버 모바일 url추가

--- 검색어키워드순위.py
+++ 검색어키워드순위.py
@@ -7,10 +7,24 @@ import ssl
 context = ssl._create_unverified_context()
 
 
-url='https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%EC%9E%90%EB%8F%99%EC%B0%A8%EB%B3%B4%ED%97%98'
-# 자동차보험 카워드 파워링크
-#url='https://comic.naver.com/webtoon/weekday.nhn'
-res=urlopen(url,context=context)
+n_w_car='https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%EC%9E%90%EB%8F%99%EC%B0%A8%EB%B3%B4%ED%97%98'
+# 네이버 웹 자동차보험 카워드
+n_w_dir='https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%EB%8B%A4%EC%9D%B4%EB%A0%89%ED%8A%B8%EC%9E%90%EB%8F%99%EC%B0%A8%EB%B3%B4%ED%97%98'
+# 네이버 웹 다이렉트자동차보험 카워드
+n_m_car='https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=%EC%9E%90%EB%8F%99%EC%B0%A8%EB%B3%B4%ED%97%98'
+# 네이버 모바일 자동차보험 카워드
+n_m_dir='https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=%EB%8B%A4%EC%9D%B4%EB%A0%89%ED%8A%B8%EC%9E%90%EB%8F%99%EC%B0%A8%EB%B3%B4%ED%97%98'
+# 네이버 모바일 다이렉트자동차보험 카워드
+d_#w_car
+# 다음 웹 자동차보험 카워드
+d_#w_dir
+# 다음 웹 다이렉트자동차보험 카워드
+d_#m_car
+# 다음 모바일 자동차보험 카워드
+d_#m_dir
+# 다음 모바일 다이렉트자동차보험 카워드
+
+res=urlopen(url_n_w_dir,context=context)
 
 #res.raise_for_status()
 
@@ -23,6 +37,7 @@ rank=soup.findAll('div',attrs={'class':'lnk_tit'})
 
 #rank2=rank1.next_sibling.next_sibling
 # next_sibling은 다음 형제관계로 넘어가는 것
+print('-'*50)
 print('기준시간:',datetime.datetime.now())
 print('키워드: 자동차보험')
 print('-'*50)
